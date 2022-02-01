d743275f601239780b7d78abd6773af47d236c51
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-02-01 13:01:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugiZadPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godina1 = 1900;
+            Console.WriteLine("Prijestupne godine: ");
+            for (int i = godina1;i < 2100;i++)
+            {
+                if(i % 400==0 || i % 4 == 0  &&  i % 100!=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
